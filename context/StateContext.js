1159bbf9133b6e00acc25ddc16a1374166fa1ee1
zadigg <62830716+zadigg@users.noreply.github.com>
2022-05-03 21:06:51 +0300

    side cart

--- context/StateContext.js
+++ context/StateContext.js
@@ -10,6 +10,9 @@ export const StateContext = ({ children }) => {
   const [totalQuantities, setTotalQuantities] = useState(0);
   const [qty, setQty] = useState(1);
 
+  let foundProduct;
+  let index;
+
   const onAdd = (product, quantity) => {
     const checkProductInCart = cartItems.find(
       (item) => item._id === product._id
@@ -39,7 +42,17 @@ export const StateContext = ({ children }) => {
     toast.success(`${qty} ${product.name} added to the cart.`);
   };
 
-  const onAddd = (product, quantity) => {};
+  const toggleCartItemQuantity = (id, value) => {
+    foundProduct = cartItems.find((item) => item._id === id);
+    index = cartItems.findIndex((product) => product._id === id);
+    if (value === "inc") {
+      setCartItems([
+        ...cartItems,
+        { ...product, quantity: product.quantity + 1 },
+      ]);
+    } else if (value === "dec") {
+    }
+  };
 
   const incQty = () => {
     setQty((prevQty) => prevQty + 1);
